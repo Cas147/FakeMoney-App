0f678f31b3f6596a3514d5c12f7d89da7f322dcb
Andres Castilla <andres@Andress-MacBook-Pro.local>
2024-04-12 17:09:06 -0500

    removed dashboard

--- src/app/dashboard/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Fragment } from "react";
-/* import sideBar from "./sideBar"; */
-
-const Page = ({ children }: { children: React.ReactNode }): JSX.Element => {
-  return <Fragment>
-    <h2>Dashboard</h2>
-  </Fragment>;
-};
-export default Page;
